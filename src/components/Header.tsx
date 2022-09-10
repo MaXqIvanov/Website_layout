a7d9729c8dbfbee12607ce6f6c9f7002afba5626
MaXqIvanov <maksivanov35@ya.ru>
2022-09-10 19:38:32 +0300

    layouts screen_1

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,20 +3,6 @@ import { Navbar, Nav, Offcanvas } from 'react-bootstrap';
 
 export const Header = () => {
   return (
-    // <Navbar bg="transparent" variant="dark" expand="x-lg" className="custom_bg">
-    //   <Navbar.Brand href="#home" className="navbar-brand"></Navbar.Brand>
-    //   <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    //   <Navbar.Collapse id="basic-navbar-nav">
-    //     <Nav className="me-auto nav">
-    //       <div className="nav-elem">FAQ</div>
-    //       <div className="nav-elem">Оплата и доставка</div>
-    //       <div className="nav-elem">Возврат</div>
-    //       <div className="nav-elem">Исследования</div>
-    //       <div className="nav-elem">Личный кабинет</div>
-    //       <div className="nav-elem">8 8 (800) 600-09-90</div>
-    //     </Nav>
-    //   </Navbar.Collapse>
-    // </Navbar>
     <Navbar bg="transparent" variant="dark" expand="xl" className="custom_bg" collapseOnSelect>
       <Navbar.Brand title="Вернуться на главную" className="main_logo_div"></Navbar.Brand>
       <Navbar.Toggle className="main_nav_button" aria-controls="offcanvasNavbar" />
--- src/views/MainView.tsx
+++ src/views/MainView.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import styles from '../scss/MainView.module.scss';
 
 export const MainView = () => {
   return (
-    <div>
-      <div>123</div>
-      <div>345</div>
+    <div className={styles.main}>
+      <div className={styles.main_screen_1}>
+        <div className={styles.screen_1_group}>
+          <div className={styles.group_text_1}>АКТИВИРУЙ ИММУНИТЕТ!</div>
+          <div className={styles.group_text_2}>
+            Всего пять секунд в день помогут укрепить иммунитет и повысить защитные силы организма
+          </div>
+          <div>dsadasdas</div>
+        </div>
+        <div className={styles.screen_1_img}></div>
+      </div>
+      <div className={styles.main_screen_2}>
+        <div>
+          <div>345</div>
+        </div>
+      </div>
     </div>
   );
 };
