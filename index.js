980015a8e8afc36083d2f04331d5d014d219eb04
MaXqIvanov <maksivanov35@ya.ru>
2022-09-11 14:06:43 +0300

    fix small bag

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-export const app = express();
+const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
